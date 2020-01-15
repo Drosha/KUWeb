4353250f6bc58beedf0e0aeb18dbdb4f429851a3
Drosha <drosha@gmail.com>
2020-01-15 21:48:23 +0300

    Additional validation for post data

--- contact.php
+++ contact.php
@@ -3,20 +3,34 @@
     $to = "info@kitchen-update.com"; // to email
     $subject = "Form Request";
 
+    $data = filter_var_array($_POST, FILTER_SANITIZE_EMAIL | FILTER_SANITIZE_STRING);
+
     //form data
-    $first_name = $_POST['name'];
-    $last_name = $_POST['surname'];
-    $phone = $_POST['phone'];
-    $from = $_POST['email'];
-    $sbj = $_POST['need'];
-    $msg = $_POST['message'];
-    $city = $_POST['city'];
-    $zip = $_POST['zip'];
+    $first_name = $data['name'];
+    $last_name = $data['surname'];
+    $phone = $data['phone'];
+    $from = $data['email'];
+    $sbj = $data['need'];
+    $msg = $data['message'];
+    $city = $data['city'];
+    $zip = $data['zip'];
 
     $message = "Name: $last_name,  $first_name. Message: $msg  \n\n Info: $from Phone: $phone Subj: $sbj \n\n $city - $zip"; 
 
     //send email
-    mail($to, $subject, $message);
+    if (
+        !empty($first_name) &&
+        !empty($last_name) &&
+        !empty($phone) &&
+        !empty($from) &&
+        !empty($sbj) &&
+        !empty($msg) &&
+        !empty($city) &&
+        !empty($zip)
+    ) {
+        mail($to, $subject, $message);
+    }
+
     //echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     
     echo '<script language="javascript">';
@@ -26,8 +40,6 @@
     echo '<script language="javascript">';
     echo 'window.history.back()';
     echo '</script>';
-    
-    
+
     exit;
-    
-?>    
+?>
